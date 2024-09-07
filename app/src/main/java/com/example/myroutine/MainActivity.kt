f1e75af0f720f257cd31d8eab13f624d147713fd
Luis Villagra <luisvillagra2000@gmail.com>
2024-09-07 10:33:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myroutine/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myroutine
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.myroutine.ui.theme.MyRoutineTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyRoutineTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/myroutine/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.myroutine.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SecondRed = Color(0xFFA72E2E)
+val SecondRedGrey = Color(0xFFB46767)
+val SecondRedGrey2 = Color(0xFF9C5B5B)
+
+val MainRed = Color(0xFFB63030)
+val MainRedGrey = Color(0xFF6D2222)
+val MainRedGrey2 = Color(0xFF835D5D)
